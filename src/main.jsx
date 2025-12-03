0504a71029237b4c9b58e6492359769f7319010a
Your Name <you@example.com>
2025-12-03 18:50:57 +0530

    Remove extra files

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import Header from './Header/Header.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header/>
   </StrictMode>,
 )
