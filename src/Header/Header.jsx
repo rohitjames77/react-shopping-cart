a83505fbfd49c63ea6b5c896197ebfe424074228
Your Name <you@example.com>
2025-12-03 18:49:15 +0530

    Create Header component within Header folder

---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <>
+    <div className='fixed w-full h-[10%] shadow-[0_0_16px_0_rgba(0,0,0,0.15)]'>
+<h1 className=' text=2xl text-red-600  '>Header </h1>
+
+    </div>
+
+    </>
+  )
+}
