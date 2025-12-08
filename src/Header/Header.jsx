71fcbec42a32297204a90d3f54a0b52a1299aaa1
Your Name <you@example.com>
2025-12-08 12:29:01 +0530

    Add isActive state & establish DropDown components & ajust stylings accordingly

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import { FaRegUser } from "react-icons/fa";
+import { RiShoppingBag4Line } from "react-icons/ri";
+import {
+  DropDownMenuMen,
+  DropDownWomen,
+  DropDownHome,
+  DropDownBeauty,
+} from "./DropDownMenu";
 
-export default function Header() {
+export default function Header({ isActive, setIsActive }) {
+  const handleMenMouseEnter = () => {
+    setIsActive({ ...isActive, men: true });
+  };
+  const handleMenMouseleave = () => {
+    setIsActive({ ...isActive, men: false });
+  };
+
+  const handleWomenMouseEnter = () => {
+    setIsActive({ ...isActive, women: true });
+    console.log(isActive);
+  };
+  const handleWomenMouseLeave = () => {
+    setIsActive({ ...isActive, women: false });
+    console.log(isActive);
+  };
+
+  const handleHomeMouseEnter = () => {
+    setIsActive({ ...isActive, home: true });
+    console.log(isActive);
+  };
+  const handleHomeMouseLeave = () => {
+    setIsActive({ ...isActive, home: false });
+    console.log(isActive);
+  };
+  const handleBeautyMouseEnter = () => {
+    setIsActive({ ...isActive, beauty: true });
+    console.log(isActive);
+  };
+  const handleBeautyMouseLeave = () => {
+    setIsActive({ ...isActive, beauty: false });
+    console.log(isActive);
+  };
+
   return (
-    <div
-      className="fixed flex flex-row w-full h-[10%] shadow-[0_0_16px_0_rgba(0,0,0,0.15)] gap-10"
+    <nav
+      className="sticky top-0 z-50 flex flex-row w-screen h-[10vh] shadow-[0_0_16px_0_rgba(0,0,0,0.15)] gap-10 bg-stone-50 "
       id="header-container"
     >
       <img
@@ -14,28 +55,84 @@ export default function Header() {
         alt="vyntra-logo"
       />
       <div
-        id="categories-link-container"
-        className=" relative mt-10 font-poppins font-bold flex gap-8 w-fit h-[50%] text-[1.8em] text-stone-400 align-end"
+        id="categories-link-container z-20"
+        className=" relative mt-10 font-poppins font-bold flex flex-row  gap-8 w-[30%] h-[50%] text-[1.1em] text-stone-400 ml-8 "
       >
-        <h2 id="men-category" className="">
-          MEN
-        </h2>
-        <h2 id="women-category" className="">
-          WOMEN
-        </h2>
-        <h2 id="electronics-category" className="">
-          ELECTRONICS
-        </h2>
-        <h2 id="home-category" className="">
-          HOME
-        </h2>
-        <h2 id="furniture-category" className="">
-          FURNITURE
-        </h2>
-        <h2 id="beauty-category" className="">
-          BEAUTY
-        </h2>
+        <div
+          onMouseEnter={handleMenMouseEnter}
+          onMouseLeave={handleMenMouseleave}
+          id="men-category-container"
+          className="relative h-[10vh] w-[5vw]"
+        >
+          <h2
+            id="men-category"
+            className="hover:border-b-4 border-red-400 w-fit hover:cursor-pointer "
+          >
+            MEN
+          </h2>
+          <DropDownMenuMen isActive={isActive} />
+        </div>
+
+        <div
+          onMouseEnter={handleWomenMouseEnter}
+          onMouseLeave={handleWomenMouseLeave}
+          id="women-category-container"
+          className="relative h-[10vh] w-[5vw] "
+        >
+          <h2
+            id="women-category"
+            className="hover:border-b-4 hover:border-fuchsia-500 w-fit hover:cursor-pointer"
+          >
+            WOMEN
+          </h2>
+          <DropDownWomen isActive={isActive} />
+        </div>
+
+        <div
+          onMouseEnter={handleHomeMouseEnter}
+          onMouseLeave={handleHomeMouseLeave}
+          id="home-category-container"
+          className="relative h-[10vh] w-[5vw] "
+        >
+          <h2
+            id="home-category"
+            className="hover:border-b-4 border-orange-500 w-fit hover:cursor-pointer"
+          >
+            HOME
+          </h2>
+          <DropDownHome isActive={isActive} />
+        </div>
+
+        <div
+          onMouseEnter={handleBeautyMouseEnter}
+          onMouseLeave={handleBeautyMouseLeave}
+          id="beauty-category-container"
+          className="relative h-[10vh] w-[5vw]"
+        >
+          <h2 id="beauty-category" className="hover:border-b-4 border-green-500 w-fit hover:cursor-pointer">
+            BEAUTY
+          </h2>
+          <DropDownBeauty isActive={isActive} />
+        </div>
       </div>
-    </div>
+
+      <div
+        id="bag-profile-container"
+        className="absolute left-[93%] flex text-[1.2em] w-[10%] font-poppins font-bold mt-8 text-stone-600  "
+      >
+        <div id="profile-container" className="flex flex-col w-[30%] ">
+          <FaRegUser className="h-[40%] w-[40%] ml-4 " />
+          <span className="text-lg font-poppins font-bold justify-center ">
+            Profile
+          </span>
+        </div>
+        <div id="bag-container" className="flex flex-col w-[50%] ">
+          <RiShoppingBag4Line className=" h-[40%] w-[40%] ml-2 " />
+          <span className=" relative left-[15%] text-lg font-poppins font-bold justify-center ">
+            Bag
+          </span>
+        </div>
+      </div>
+    </nav>
   );
 }
