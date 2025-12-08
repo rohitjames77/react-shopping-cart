79350fcef17ab8c2281ac50be59da615f0c8bf7a
Your Name <you@example.com>
2025-12-08 12:35:11 +0530

    Place App component as single parent to render

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import Header from './Header/Header.jsx'
-
+import ContentPage from './Content/Content.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Header/>
+ <App/>
   </StrictMode>,
 )
