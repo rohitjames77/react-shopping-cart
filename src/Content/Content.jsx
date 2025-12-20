8c9b27b16fafb83135adf56ebbd6089a777e6fbc
Your Name <you@example.com>
2025-12-20 12:09:36 +0530

    Add error handling logic toasync fetching block

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,14 +1,52 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import Products from "../products/Products";
 
 function ContentPage({ isActive }) {
   console.log(isActive);
-  
-  
+  const [storeApiData, setStoreApiData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        const response = await fetch("https://fakestoreapi.com/products");
+        const result = await response.json();
+        setStoreApiData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false); // always runs
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
     return (
+      <div className="flex items-center justify-center h-64">
+        <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+      </div>
+    );
+  }
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-64 text-red-600 font-semibold">
+        Error: {error}
+      </div>
+    );
+  }
+
+  return (
     <div
       id="content-page"
-      className={`relative right-0 left-0 w-screen h-screen overflow-y-auto bg-gray-50 ${isActive.men ||isActive.women ||isActive.home ||isActive.beauty === true ? "bg-gray-400/25" : "bg-gray-50"}`}
-    ></div>
+      className={`relative right-0 left-0 w-screen h-[200%] overflow-y-auto ${isActive.men || isActive.women || isActive.home || isActive.beauty === true ? "bg-gray-400/25" : "bg-gray-50"}`}
+    >
+      <Products storeApiData={storeApiData} setStoreApiData={setStoreApiData} />
+    </div>
   );
 }
 
