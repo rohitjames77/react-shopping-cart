4590870d11d15b245b4872cfb4d3a3c9634aa063
Your Name <you@example.com>
2025-12-20 13:57:33 +0530

    Add paypal login notification

--- src/Cart/CartCheckoutDeliveryDetails.jsx
+++ src/Cart/CartCheckoutDeliveryDetails.jsx
@@ -4,6 +4,22 @@ import { FaGift } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export default function CartCheckOut() {
+  const [logInSuccess, setLogInSuccess] = useState(false);
+  const [isVisaSelected, setIsVisaSelected] = useState(false);
+  const [isPaypalSelected, setIsPaypalSelected] = useState(false);
+
+  const handleVisaClick = () => {
+    setIsVisaSelected(true);
+    setIsPaypalSelected(false);
+  };
+  const handlePaypalClick = () => {
+    setIsVisaSelected(false);
+    setIsPaypalSelected(true);
+  };
+
+  const handleBtnOnClick = () => {
+    setLogInSuccess(true);
+  };
   return (
     <div
       id="Cart-container"
@@ -24,6 +40,12 @@ export default function CartCheckOut() {
         id="cart-content"
         className="h-[80%] w-full grid grid-cols-6 grid-rows-6 bg-gray-100 gap-4"
       >
+        <div
+          class={`row-start-2 col-start-2 col-end-4 h-[35%] w-[70%] rounded-xl flex items-center gap-2 bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded animate-pulse delay-3 ${logInSuccess ? "block" : "hidden"} `}
+        >
+          <span class="text-xl">✔</span>
+          <span class="font-medium">Successfully logged in</span>
+        </div>
         <div
           id="nav-header"
           className="row-start-1 col-start-1 col-end-5"
@@ -159,13 +181,16 @@ export default function CartCheckOut() {
                   className="row-start-3 col-start-2 h-[150%] ml-2 "
                 />
               </div>
-              <button type="" className=" relative left-[55%] bottom-[8%] h-[5vh] w-[20vw] row-start-3 row-end-6 col-start-4 col-end-8 bg-blue-500 text-white font-bold font-poppins rounded-lg p-2 active:translate-y-[4px] active:shadow-lg active:bg-blue-800 ">Proceed to Payments</button>
+              <button
+                type=""
+                className=" relative left-[55%] bottom-[8%] h-[5vh] w-[20vw] row-start-3 row-end-6 col-start-4 col-end-8 bg-blue-500 text-white font-bold font-poppins rounded-lg p-2 active:translate-y-[4px] active:shadow-lg active:bg-blue-800 "
+              >
+                Proceed to Payments
+              </button>
             </form>
           </div>
         </div>
-]        
-        
-        
+
         <div
           id="order-summary"
           className="shadow-2xl row-start-2 row-end-7 w-[80%] h-[50%] col-start-5 col-end-7 p-6 mt-8 mr-8  bg-white rounded-xl"
@@ -180,12 +205,21 @@ export default function CartCheckOut() {
             >
               Order Summary
             </h1>
-            
-            </div>
-            <div id="total-price-discount-container" className="flex flex-col order-2 ">
-              <div id="cart-quantity-heading-container" className="grid grid-rows-1 grid-cols-6">
-                <AiOutlineShoppingCart className=" row-start-1 col-start-1 h-1/2 justify-self-end place-self-center" /> 
-              <span id="item-quantity-heading" className=" row-start-1 col-start-2 col-end-5 text-lg">Item(s) in a cart
+          </div>
+          <div
+            id="total-price-discount-container"
+            className="flex flex-col order-2 "
+          >
+            <div
+              id="cart-quantity-heading-container"
+              className="grid grid-rows-1 grid-cols-6"
+            >
+              <AiOutlineShoppingCart className=" row-start-1 col-start-1 h-1/2 justify-self-end place-self-center" />
+              <span
+                id="item-quantity-heading"
+                className=" row-start-1 col-start-2 col-end-5 text-lg"
+              >
+                Item(s) in a cart
               </span>
               <span
                 id="details-title"
@@ -198,27 +232,69 @@ export default function CartCheckOut() {
               id="price-tax-container"
               className="border-b-4 border-gray-400 border-t-4 grid grid-rows-5 grid-cols-3 w-[90%] ml-4 p-2 font-poppins"
             >
-              <span id="sub-total-heading" className="row-start-1">Order Subtotal</span>
-              <span id="sub-total-quantity" className="row-start-1 col-start-3">$SubTotal</span>
-              <span id="discount-heading" className="row-start-2 col-start-1">Sale Discount</span>
-              <span id="discount-quantity" className="row-start-2 col-start-3">$ Sale discount</span>
-              <span id="net-subtotal-heading" className="row-start-3 col-start-1 col-end-3 font-bold">Net Order Subtotal</span>
-              <span id="net-subtotal-quantity" className="row-start-3 col-start-3">$</span>
-              <span id="shipping-heading" className="row-start-4 col-start-1">Shipping</span>
-              <span id="shipping-price" className="row-start-4 col-start-3">$shipping price</span>
-              <span id="pre-tax-total-heading" className="row-start-5 col-start-1 font-bold">Pre-Tax Total</span>
-              <span id="pre-tax-total-quantity" className="row-start-5 col-start-3">$</span>
+              <span id="sub-total-heading" className="row-start-1">
+                Order Subtotal
+              </span>
+              <span id="sub-total-quantity" className="row-start-1 col-start-3">
+                $SubTotal
+              </span>
+              <span id="discount-heading" className="row-start-2 col-start-1">
+                Sale Discount
+              </span>
+              <span id="discount-quantity" className="row-start-2 col-start-3">
+                $ Sale discount
+              </span>
+              <span
+                id="net-subtotal-heading"
+                className="row-start-3 col-start-1 col-end-3 font-bold"
+              >
+                Net Order Subtotal
+              </span>
+              <span
+                id="net-subtotal-quantity"
+                className="row-start-3 col-start-3"
+              >
+                $
+              </span>
+              <span id="shipping-heading" className="row-start-4 col-start-1">
+                Shipping
+              </span>
+              <span id="shipping-price" className="row-start-4 col-start-3">
+                $shipping price
+              </span>
+              <span
+                id="pre-tax-total-heading"
+                className="row-start-5 col-start-1 font-bold"
+              >
+                Pre-Tax Total
+              </span>
+              <span
+                id="pre-tax-total-quantity"
+                className="row-start-5 col-start-3"
+              >
+                $
+              </span>
             </div>
-           
           </div>
-           <div id="total-saving-container"className="grid grid-rows-1 grid-cols-3  ">
-              <span id="total-saving-heading" className="row-start-1 col-start-2 justify-self-start text-xl font-bold ">Total Savings</span>
-              <span id="total-saving-quantity" className="row-start-1 col-start-3 justify-self-start text-lg text-red-600 font-bold  ">$</span>
-            </div>
+          <div
+            id="total-saving-container"
+            className="grid grid-rows-1 grid-cols-3  "
+          >
+            <span
+              id="total-saving-heading"
+              className="row-start-1 col-start-2 justify-self-start text-xl font-bold "
+            >
+              Total Savings
+            </span>
+            <span
+              id="total-saving-quantity"
+              className="row-start-1 col-start-3 justify-self-start text-lg text-red-600 font-bold  "
+            >
+              $
+            </span>
+          </div>
         </div>
       </div>
     </div>
   );
 }
-
-
